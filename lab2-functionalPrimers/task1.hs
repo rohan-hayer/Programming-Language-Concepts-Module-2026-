fb081189aaea5e2e57f6df766084dfd0a7ac8ceb
rohan-hayer <rohan.singh.hayer@gmail.com>
2026-04-05 14:59:40 +0100

    Add files via upload

---
+++ lab2-functionalPrimers/task1.hs
@@ -0,0 +1,11 @@
+module Main where
+
+main =
+do
+    print (myFunc1 5)
+    print (myFunc2 10)
+    print thirdFunc
+
+myFunc1 x = x*10
+myFunc2 x = x*2
+thirdFunc = "Hello, this is 3rd Func"
---
+++ lab2-functionalPrimers/task3.hs
@@ -0,0 +1,15 @@
+sgn x = if x < 0 
+          then -1 
+          else if x == 0 
+              then 0 
+              else 1
+
+sgnGuards x | x < 0 = -1
+            | x == 0 = 0
+            | otherwise = 1
+
+main = do
+    putStrLn "Please enter a number"
+    input <- getLine 
+    let x = (read input :: Int)
+    putStrLn( show (sgn (x)) )
